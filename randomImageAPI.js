263b8092e92a3e2640c247a11c8deedb44311b7f
Robin van den Bosch <robin.van.den.bosch@gmail.com>
2021-04-07 12:45:00 +0200

    Update randomImageAPI.js

--- randomImageAPI.js
+++ randomImageAPI.js
@@ -1,7 +1,11 @@
-var refreshImage = setInterval(function() {
-    var areaWidth = Math.round(parseInt(document.getElementById('images').offsetWidth)/100)*100;
-    var areaHeight = Math.round(parseInt(document.getElementById('images').offsetHeight)/100)*100;
-    var loadImage = 'https://picsum.photos/' + areaWidth + '/' + areaHeight;
-    // API voor random Unsplash foto's
-    document.getElementById('randomImageContainer').innerHTML =  '<img id=\"randomImage\" src=\"' + loadImage + '?random=10\" \/>';
-}, 2000);
+function getRandomImage()  {
+    setInterval(function() {
+        var areaWidth = Math.round(parseInt(document.getElementById('images').offsetWidth)/100)*100;
+        var areaHeight = Math.round(parseInt(document.getElementById('images').offsetHeight)/100)*100;
+        var loadImage = 'https://picsum.photos/' + areaWidth + '/' + areaHeight;
+        // API voor random Unsplash foto's
+        document.getElementById('randomImageContainer').innerHTML =  '<img id=\"randomImage\" src=\"' + loadImage + '?random=10\" \/>';
+    }, 2000);
+}
+
+getRandomImage();
